f5bf32be279ba9165c42023e9e945358e7aac3cd
kmarques <kmarques@student.42sp.org.br>
2021-06-18 21:27:57 +0000

    correção na ex03 da C07 e C08

---
+++ C/8/ex01/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmarques <kmarques@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/13 14:22:33 by vgoncalv          #+#    #+#             */
+/*   Updated: 2021/06/18 16:26:28 by kmarques         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_boolean.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+t_bool	ft_is_even(int nbr)
+{
+	return ((EVEN(nbr)) ? TRUE : FALSE);
+}
+
+int		main(int argc, char *argv[])
+{
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
+}
---
+++ C/8/ex02/ft_abs.h
@@ -0,0 +1,5 @@
+#ifndef FT_ABS_H
+# define FT_ABS_H
+
+# define ABS(value) return value < 0 ? -value : value;
+#endif
